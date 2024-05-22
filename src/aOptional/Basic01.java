93226fc6f5b375e4c5c0da641927356d8428bdb8
doriver <tkdduq241@naver.com>
2024-05-22 21:39:00 +0900

    Optional 잠깐 봄

--- src/aOptional/Basic01.java
+++ src/aOptional/Basic01.java
@@ -1,10 +1,22 @@
 package aOptional;
 
+import java.util.Optional;
+
 public class Basic01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("aa");
+		// Optional 객체 생성
+        Optional<String> optional = Optional.of("Hello, World!");        
+//        Optional<String> nnnns = Optional.of(null); // NullPointerException
+        
+        Optional<String> emptyOptional = Optional.empty();
+        
+        System.out.println(optional); 
+        // Optional[Hello, World!] , Optional의 value값이 "Hello, World!"
+        
+        System.out.println(emptyOptional);
+        // Optional.empty , Optional의 value값이 null
+
 	}
 
 }
