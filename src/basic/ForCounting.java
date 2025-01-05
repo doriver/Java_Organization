29e78ebf17d8b899d59c9ffd0bce5a0315cc6273
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2025-01-05 23:38:05 +0900

    ..

---
+++ src/basic/ForCounting.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class ForCounting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 0; i < 5; i+=2) { // i 2씩 증가
+			System.out.println(i);
+		}
+	}
+
+}
